8bf1b07031a884ddcc95c3c3ccd97688d97b98d1
SvAb9 <sarav.acostab@uqvirtual.edu.co>
2025-10-26 15:51:10 -0500

    feat: agregar atributos y métodos a las clases Arista y AdministradorRepository
    Se añadieron los nuevos atributos necesarios y se implementaron los métodos principales en ambas clases para ampliar su funcionalidad dentro del sistema.

--- src/main/java/edu/universidad/modelo/Arista.java
+++ src/main/java/edu/universidad/modelo/Arista.java
@@ -5,4 +5,25 @@
 package edu.universidad.modelo;
 
 public class Arista {
+    private String origenId;
+    private String destinoId;
+    private double peso;
+
+    public Arista(){}
+
+    public Arista(String origenId, String destinoId, double peso) {
+        this.origenId = origenId;
+        this.destinoId = destinoId;
+        this.peso = peso;
+    }
+
+    public String getOrigenId() {
+        return origenId;
+    }
+    public String getDestinoId() {
+        return destinoId;
+    }
+    public double getPeso() {
+        return peso;
+    }
 }
--- src/main/java/edu/universidad/vista/VentanaPrincipal.java
+++ src/main/java/edu/universidad/vista/VentanaPrincipal.java
@@ -1,6 +1,6 @@
 /**
  * Interfaz principal del sistema, aqui el ususario podra ver todas la ventanas
- * mas adelante se agregaran mas vistas segun convenga
+ * mas adelante se agregaran mas vistas segun convenga.
  */
 package edu.universidad.vista;
 
