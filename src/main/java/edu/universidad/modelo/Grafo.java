8a0946c6971243075cdb400950a70fbb68fd3d63
Sara <131993370+Smoe111@users.noreply.github.com>
2025-10-28 22:21:06 -0500

    feat: metodos de la clase grafo agregados Mirar metodos de nodo creados Falta creacion de metodos de la clase Arista Documentacion de metodos

--- src/main/java/edu/universidad/modelo/Grafo.java
+++ src/main/java/edu/universidad/modelo/Grafo.java
@@ -4,5 +4,82 @@
  */
 package edu.universidad.modelo;
 
+import java.util.*;
+
 public class Grafo {
+
+    private Map<String,Nodo> nodos= new LinkedHashMap<>();
+    private List<Arista> aristas=new ArrayList<>();
+
+    public void agregarNodo (Nodo n){
+        nodos.put(n.getId(), n);
+    }
+    public void agregarArista( String origen, String destino, double peso) {
+        Nodo org= nodos.get(origen), dest= nodos.get(destino);   // ambos son de tipo nodo
+        if (org == null || dest == null) return;
+        aristas.add(new Arista (origen, destino, peso));
+        org.addVecino(destino, peso);
+    }
+
+    public Collection<Nodo> getNodos(String id){
+        return nodos.values();
+    }
+
+    public List <Arista> getAristas () {
+        return aristas;
+    }
+
+    public Nodo getNodo (String id){
+        return nodos.get(id);
+    }
+
+    public List <Nodo> dijkstra (String origenId, String destinoId){   // Encuentra el camino más corto entre dos nodos
+        if(!nodos.containsKey(origenId) || !nodos.containsKey(destinoId)) return Collections.emptyList();
+        // si alguno de los dos nodos no existe, retorna una lista vacia
+        Map<String, Double> distancia= new HashMap<>();
+        Map<String, String> previo= new HashMap<>();
+        for (String id: nodos.keySet()){
+            distancia.put(id, Double.POSITIVE_INFINITY);  // las distancias se inicializan como infinitas
+            previo.put(id, null); // los nodos previos se inicializan en nulos
+        }
+        distancia.put(origenId, 0.0);
+        PriorityQueue<String> masCorta= new PriorityQueue<>(Comparator.comparingDouble(distancia:: get));
+        masCorta.add(origenId);
+        //la cola procesa primero el nodo con la distancia más corta
+
+        while(!masCorta.isEmpty()){
+            String nodo= masCorta.poll();  // extrae el nodo con menor distancia
+            if(nodo.equals(destinoId)) break;  // si llegamos al destino se termina el bucle
+
+            Nodo n= nodos.get(nodo);
+            for(var e : n.getVecinos().entrySet()){  //revisa a todos los vecinos
+                String id = e.getKey();  // id del vecino
+                double peso= e.getKey();  // peso de la arista
+                double alt= distancia.getKey(nodo) + peso;  //distancia alternativa
+
+                if(alt<distancia.get(id)){  // si el camino es mas corto
+                    distancia.put(id, alt);  // actualizamos la distancia
+                    previo.put(id, nodo);  // actualizamos el nodo anterior
+                    masCorta.remove(id);   // borramos el nodo anterior
+                    masCorta.add(id);  //agregamos el nuevo nodo encontrado
+                }
+            }
+        }
+
+        LinkedList<Nodo>  camino= new LinkedList<>();
+        String d= destinoId;
+        if (previo.get(d) == null && !d.equals(origenId)){
+            if(d.equals(origenId))
+                camino.add(nodos.get(d));
+                return camino;
+        // verifica si hay un camino valido
+        }
+
+        while (d != null){
+            camino.addFirst(nodos.get(d));
+            d = previo.get(d);
+        // construye el camino encontrados desde el destino hasta el origen
+        }
+        return camino;
+    }
 }
